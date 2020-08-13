faf6f1f0de9b1c33f74dee0035be82965a1cd826
unknown <barakfriberg2@gmail.com>
2020-08-13 11:20:06 +0300

    finish day 4

---
+++ project/day 4/main1.js
@@ -0,0 +1,81 @@
+
+// const tellAJoke = function(){
+//   console.log("something funny")
+// }
+
+// tellAJoke()
+
+// ------------------------------------------------------
+// const greet = function(name){
+//   console.log("Hello there, " + name)
+// }
+// greet('barak')
+
+// const names = ["Mr. Woods", "Sir Newton", "Lady Gamora", "Her Highness Kayla", "Master Shen", "Squire Carl", "The Eternal Brittany"]
+
+// for(let n of names){
+//   greet(n)
+// }
+// ----------------------------------------------------------
+  
+
+// const greet = function(name, timeOfDay){
+//   console.log("Good " + timeOfDay + ", " + name)
+// }
+// greet("Darrell", "Evening")
+
+// ------------------------targil 1---------------------------------
+ 
+// const calcAge = function(x,y){
+//  let age = (x-y)
+//  return (age)
+// }
+// const age = calcAge(2017, 1989)
+
+// console.log (age)
+
+
+// ---------------------------targil 2-----------------------  
+
+
+// const calcAge = function(x,y){
+//  let age = (x-y)
+//  return (age)
+// }
+// const age = calcAge(2018, 2016)
+
+// console.log (age-1 +"or"+ age)
+
+
+// ------------------------targil 3-------------------
+ 
+// const iseven = function(x){
+//   return x % 2 === 0
+// }
+
+// console.log(iseven(4)) 
+
+
+
+// ---------------------targil 4--------------------
+
+// const arry  = [2, 4, 0, 100, 4, 11, 2602, 36]
+
+// const iseven = function(num){
+//     if (num % 2 === 0)
+//   console.log (num)
+//    }
+//   for (a of arry){
+//     iseven(a)
+//   }
+
+
+//   ------------------targil 5-----------------------
+
+const integers 
+
+const arry
+
+
+
+
